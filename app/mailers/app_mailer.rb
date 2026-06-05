5df5e6a992ef3a8c74c6371554ab3b233ba580ae
Mạnh Hưng <buimanhhung3105@gmail.com>
2026-06-05 12:11:40 +0700

    feat: customize email templates and application branding

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -6,32 +6,32 @@ class AppMailer < ApplicationMailer
     @domain = domain
     @email_address = email_address
     @user = user
-    mail to: email_address, subject: "Verify your ownership of #{@domain.name}"
+    mail to: email_address, subject: "Xác minh quyền sở hữu #{@domain.name}"
   end
 
   def password_reset(user, return_to = nil)
     @user = user
     @return_to = return_to
-    mail to: @user.email_address, subject: "Reset your OmmicomMail password"
+    mail to: @user.email_address, subject: "Đặt lại mật khẩu OmmicomMail"
   end
 
   def server_send_limit_approaching(server)
     @server = server
-    mail to: @server.organization.notification_addresses, subject: "[#{server.full_permalink}] Mail server is approaching its send limit"
+    mail to: @server.organization.notification_addresses, subject: "[#{server.full_permalink}] Máy chủ mail sắp đạt giới hạn gửi"
   end
 
   def server_send_limit_exceeded(server)
     @server = server
-    mail to: @server.organization.notification_addresses, subject: "[#{server.full_permalink}] Mail server has exceeded its send limit"
+    mail to: @server.organization.notification_addresses, subject: "[#{server.full_permalink}] Máy chủ mail đã vượt quá giới hạn gửi"
   end
 
   def server_suspended(server)
     @server = server
-    mail to: @server.organization.notification_addresses, subject: "[#{server.full_permalink}] Your mail server has been suspended"
+    mail to: @server.organization.notification_addresses, subject: "[#{server.full_permalink}] Máy chủ mail đã bị tạm ngưng"
   end
 
   def test_message(recipient)
-    mail to: recipient, subject: "OmmicomMail SMTP Test Message"
+    mail to: recipient, subject: "Email kiểm tra SMTP từ OmmicomMail"
   end
 
 end
